c592b365f0eb0e4fa090b4cf2c083c3e0343565e
djramic <58434250+djramic@users.noreply.github.com>
2020-10-13 15:31:20 +0200

    ADDED: cmds, basic function

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,16 +5,22 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.Compression;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
 
 
 public class main extends ListenerAdapter {
     private static String token = "NzY1NTI4OTg0MzUxMTQ1OTg0.X4WIbA.C13ZCCfxp7wLqGmFkWKEE0KD7YY";
+    private static String admin_id = "335178837228912651";
+    private ArrayList<String> land_list = new ArrayList<>();
+    private ArrayList<String> air_list = new ArrayList<>();
+    private ArrayList<String> urban_list = new ArrayList<>();
     public static void main(String args[]) {
 
         JDABuilder builder = JDABuilder.createDefault(token).addEventListeners(new main());
@@ -29,16 +35,58 @@ public class main extends ListenerAdapter {
 
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        if (event.isFromType(ChannelType.PRIVATE))
-        {
-            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
-                    event.getMessage().getContentDisplay());
+        Message msg = event.getMessage();
+        String msg_arr[] = msg.getContentRaw().split(" ",2);
+        System.out.println("Komanda je : " + msg_arr[0]);
+        //System.out.println("Poruka je  : " + msg_arr[1]);
+
+        if(msg_arr[0].toLowerCase().equals("!land")){
+            System.out.println("ID korisnika koji pise: " + event.getAuthor().getId());
+            if(event.getAuthor().getId().equals(admin_id))
+            {
+                land_list.add(msg_arr[1]);
+                System.out.println("dodao sam u listu land");
+            }
+            else
+            {
+                event.getChannel().sendMessage("Komanda je dostupna samo adminu").queue();
+            }
         }
-        else
-        {
-            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
-                    event.getTextChannel().getName(), event.getMember().getEffectiveName(),
-                    event.getMessage().getContentDisplay());
+        if(msg_arr[0].toLowerCase().equals("!air")){
+            System.out.println("ID korisnika koji pise: " + event.getAuthor().getId());
+            if(event.getAuthor().getId().equals(admin_id))
+            {
+                air_list.add(msg_arr[1]);
+            }
+            else
+            {
+                event.getChannel().sendMessage("Komanda je dostupna samo adminu").queue();
+            }
+        }
+        if(msg_arr[0].toLowerCase().equals("!urban")){
+            System.out.println("ID korisnika koji pise: " + event.getAuthor().getId());
+            if(event.getAuthor().getId().equals(admin_id))
+            {
+                urban_list.add(msg_arr[1]);
+            }
+            else
+            {
+                event.getChannel().sendMessage("Komanda je dostupna samo adminu").queue();
+            }
+        }
+
+        if(msg_arr[0].toLowerCase().equals("!do")){
+            event.getChannel()
+                    .sendMessage("URBAN:\n" + urban_list + "\nAIR:\n" + air_list +"\nLAND:\n" + land_list)
+                    .queue();
+        }
+        if(msg_arr[0].toLowerCase().equals("!clear")){
+            urban_list.clear();
+            air_list.clear();
+            land_list.clear();
+            event.getChannel()
+                    .sendMessage("Liste su obrisane")
+                    .queue();
         }
     }
 }
